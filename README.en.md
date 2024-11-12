7ed692ff2aed53aefa861cbf52b473417c02b2dc
EillesWan <W-YI_DoctorYI@outlook.com>
2024-11-13 01:55:45 +0800

    第一次提交哦~

---
+++ README.en.md
@@ -0,0 +1,9 @@
+# 瞽字象形 PyDrawille
+
+Using Unicode braille characters to draw pixel graphics in a terminal.
+
+此自述文件亦可见于诸下文字\
+The README is also in\
+[🇨🇳 Chinese 汉语](./README.md)
+
+The aim of the library's development is to avoid the [drawille](https://github.com/asciimoo/drawille) library's [AGPLv3](https://github.com/asciimoo/drawille/blob/master/LICENSE) license trapping. We use a totally different logic to achieve the same functionality, in the hope of creating a fully independent and free software. To preserve the original source's independence, we did not use the API of drawille library, and did not use the same code logic, allowing a completely new way to present the braille characters' graphics.
---
+++ test_draw.py
@@ -0,0 +1,36 @@
+from PIL import Image, ImageFont
+import numpy as np
+from PyDrawille import CanvasSurface
+
+canvas = CanvasSurface()
+
+arr = np.arange(canvas.surface_width)
+yarr = np.intc(np.cos(arr / 10) * canvas.surface_height / 3 + canvas.surface_height / 2)
+
+# print(yarr)
+
+mxg = min(canvas.surface_width, canvas.surface_height)
+mxgarr = np.arange(mxg)
+
+canvas.set_points(zip(mxgarr, mxgarr))
+canvas.set_line(canvas.surface_height // 2)
+
+canvas.set_pixels(
+    arr,
+    yarr,
+)
+# canvas.set_pixels(range(80,),range(40,))
+
+print(canvas.dump())
+
+canvas.dump_image(
+    ImageFont.truetype(
+        font="./DejaVuSansCondensed.ttf",
+        size=32,
+        encoding="utf-8"
+    ),
+    0,
+    255,
+).save(
+    "test.png",
+)
